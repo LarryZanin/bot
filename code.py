ac078fd32edb67eb10ad40a5a6d31103f8db5b4f
LarryZanin <98313217+LarryZanin@users.noreply.github.com>
2024-10-19 12:29:19 +0300

    добавил больше слов и отправку админу

--- code.py
+++ code.py
@@ -2,10 +2,13 @@ from telegram import Update, ChatPermissions
 from telegram.ext import Updater, MessageHandler, filters, CallbackContext
 
 # Список запрещённых слов и фраз, которые могут указывать на рекламу
-BLOCKED_KEYWORDS = ['скидка', 'акция', 'купить', 'реклама', 'sale', 'http', 'www']
+BLOCKED_KEYWORDS = ['работа', 'рабочие','разнорабочие','рабочих','разнорабочих','стажировка','стажировки','стажировку','научу','обучу','казино','Казино','КАЗИНО','регистрации', 'выигрыш', 'выигрыша', 'реклама','t.me','долги','долгах','должник','срочно','с деньгами','финансовые проблемы', 'sale', 'http', 'www']
 
 # Список пользователей, которых не нужно блокировать
 EXCLUDED_USERS = [1569044523, 5284524433]  # ID пользователей, которых нельзя блокировать
+
+# ID администратора, которому нужно отправлять сообщения
+ADMIN_ID = 1569044523  # ID пользователя, которому бот будет отправлять сообщения
 
 # Функция для проверки сообщений на наличие рекламы
 def filter_ads(update: Update, context: CallbackContext) -> None:
@@ -23,13 +26,14 @@ def filter_ads(update: Update, context: CallbackContext) -> None:
         context.bot.kick_chat_member(chat_id, user_id)
 
         # Отправляем сообщение о блокировке в чат
-        context.bot.send_message(chat_id, f"Пользователь {update.message.from_user.full_name} был заблокирован за спам.")
+        # context.bot.send_message(chat_id, f"Пользователь {update.message.from_user.full_name} был заблокирован за спам.")
 
         # Логируем сообщение о спаме (или отправляем сообщение о спаме в другой чат)
-       # context.bot.send_message(chat_id, f"Пользователь {update.message.from_user.full_name} ({user_id}) заблокирован за рекламу.")
-
-        # Можно также уведомить администратора о блокировке или отправить "жалобу"
-        # (это опционально, т.к. отправка жалоб на спам напрямую не поддерживается API)
+        # context.bot.send_message(chat_id, f"Пользователь {update.message.from_user.full_name} ({user_id}) заблокирован за рекламу.")
+        
+        # Пересылаем текст сообщения администратору
+        context.bot.send_message(ADMIN_ID, f"Заблокированный пользователь: {update.message.from_user.full_name} (ID: {user_id})\n"
+                                           f"Текст сообщения: {update.message.text}")
 
 def main():
     # Используйте токен вашего бота
